3ad85fb072c1fa5caa75e4e396e93d282c869a23
vagrant <vagrant@homestead>
2020-07-10 03:21:18 +0000

    显示个人资料

--- routes/web.php
+++ routes/web.php
@@ -37,6 +37,6 @@ Route::post('email/resend', 'Auth\VerificationController@resend')->name('verific
 
 Route::get('/users/{user}', 'UsersController@show')->name('users.show');
 Route::get('/users/{user}/edit', 'UsersController@edit')->name('users.edit');
-Route::patch('/users/{user}', 'UsersController@update')->name('users.update');
+Route::put('/users/{user}', 'UsersController@update')->name('users.update');
 
 
